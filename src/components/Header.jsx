abaecdbbfa3b01f664fbbae725bce38313b4590e
= <=>
2022-05-09 18:20:11 +0530

    push code

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  const logo = (
+    <img
+      
+    />
+  );
+  return (
+    <div className="header">
+      {logo}
+      <h1>  HAVE A PLAN </h1>
+    </div>
+  );
+}
+
+export default Header;
